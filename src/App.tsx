d3515bb1cbef4bdf221fa9f606036754ed2d930f
aydikstudio <aydikstudio@mail.ru>
2024-01-17 18:43:29 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,97 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SearchOutlined } from '@material-ui/icons';
-import './App.scss';
-import Home from './pages/Home/Home';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { FormControl, Grid, IconButton, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+// @ts-nocheck
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SearchOutlined } from "@material-ui/icons";
+import "./App.scss";
+import Home from "./pages/Home/Home";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import {
+  FormControl,
+  Grid,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import axios from 'axios';
+import { Pokedex } from "./interfaces/books";
 
 function App() {
+
+  const [books, setBooks] = useState<Pokedex[]>([])
+  const [search, setSearch] = useState<string>('')
+
+  const sendSearch = async() => {
+    await axios({
+      method: 'get',
+      url: process.env.REACT_APP_SOURCE+"?q="+search,
+      responseType: 'stream'
+    })
+      .then(function (response) {
+        let book = JSON.parse(response.data);
+        setBooks(book.items)
+      });
+  }
+
 
 
   return (
     <Box className="App">
       <Box className="header">
-      <Typography variant="h1" component="h1" >
-        Search for books
-</Typography>
-   
-<TextField
-                className='header-input'
-                id="standard-bare"
-                variant="outlined"
-                defaultValue="How can we help"
-                sx={{
-                  width: 500
-                }}
-                InputProps={{
-                  endAdornment: (
-                    <IconButton>
-                      <SearchOutlined />
-                    </IconButton>
-                  ),
-                }}
-              />
+        <Typography variant="h1" component="h1">
+          Search for books
+        </Typography>
 
+        <TextField
+          className="header-input"
+          id="standard-bare"
+          variant="outlined"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+          sx={{
+            width: 500,
+          }}
+          InputProps={{
+            endAdornment: (
+              <IconButton>
+                <SearchOutlined onClick={() => sendSearch()} />
+              </IconButton>
+            ),
+          }}
+        />
 
-
-
-      <Box className="header-inputs-block">
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-    Categories
-    <select name="pets" id="pet-select" className="header-input-block">
-  <option value="">--Please choose an option--</option>
-  <option value="dog">Dog</option>
-  <option value="cat">Cat</option>
-  <option value="hamster">Hamster</option>
-  <option value="parrot">Parrot</option>
-  <option value="spider">Spider</option>
-  <option value="goldfish">Goldfish</option>
-</select>
-
-        </Grid>
-        <Grid item xs={6}>
-        Sorting By
-        <select name="pets" id="pet-select" className="header-input-block">
-  <option value="">--Please choose an option--</option>
-  <option value="dog">Dog</option>
-  <option value="cat">Cat</option>
-  <option value="hamster">Hamster</option>
-  <option value="parrot">Parrot</option>
-  <option value="spider">Spider</option>
-  <option value="goldfish">Goldfish</option>
-</select>
-        </Grid>
-    
-      </Grid>
-      </Box>
-   
+        <Box className="header-inputs-block">
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              Categories
+              <select className="header-input-block">
+                <option value="all">all</option>
+                <option value="art">art</option>
+                <option value="biography">biography</option>
+                <option value="computers">computers</option>
+                <option value="history">history</option>
+                <option value="medical">medical</option>
+                <option value="poetry">poetry</option>
+              </select>
+            </Grid>
+            <Grid item xs={6}>
+              Sorting By
+              <select className="header-input-block">
+                <option value="relevance">relevance</option>
+                <option value="newest">newest</option>
+              </select>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter> 
+        <Routes>
+          
+          <Route path="/" element={<Home items={{books}} />} />
+        </Routes>
+      </BrowserRouter>
     </Box>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,53 @@
+// @ts-nocheck
 import { FC } from "react";
+import { Pokedex } from "../../interfaces/books";
+import { Box, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
 
+const Home: FC<Pokedex[]> = (items) => {
+  let books = items.items.books;
 
-const Home:FC = () => {
-    return(<div>Home</div>)
-}
+  return (
+    <Box>
+      <Box >Кол-во: {books.length}</Box>
+      {books.length > 0 && (
+   <Box>
+   <Container className="mt-20">
+
+   <Grid container spacing={2}>
+    {books.map((item) => {
+        console.log(item)
+        return (
+             <Grid item  md={4}>
+            <Card>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        width='140'
+        image={item.volumeInfo.imageLinks.smallThumbnail}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+     
+     </Card>
+            </Grid>
+        )
+    })}
+
+   </Grid>
+   </Container>
+ </Box>
+      )}
+   
+    </Box>
+  );
+};
 
 export default Home;
